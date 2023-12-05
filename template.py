468318b71c15a026bff5ce75f4973552d0e9196a
Hari <harihararajanrm.cms@gmail.com>
2023-12-05 20:52:20 +0100

    project template added

---
+++ template.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+
+folder_list =[
+    "config.yaml",
+    "params.yaml"
+    "src/entity/__init__.py",
+    "src/config/__init__.py",
+    "src/components/__init__.py",
+    "src/pipeline/__init__.py",
+    "src/utils/__init__.py",
+    "src/utils/common.py",
+    "main.py",
+    "dvc.yaml",
+]
+
+for foldername in folder_list:
+    path_dirname = os.path.dirname(foldername)
+    path_base_name = os.path.basename(foldername)
+    print(path_dirname, path_base_name)
+    os.makedirs(os.path.join(os.getcwd(),path_dirname), exist_ok=True)    
+    with open(foldername, 'w') as f:
+        pass
+        
