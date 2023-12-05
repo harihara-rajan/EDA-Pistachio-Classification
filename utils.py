529fbf60330914c4fd17875d5f0a265f3267fca3
Hari <harihararajanrm.cms@gmail.com>
2023-12-05 16:25:17 +0100

    distribution based on class

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ import os
 import kaggle
 from pathlib import Path
 def download(copy_from_url:str, path:Path)->None:
-    os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
     kaggle.api.dataset_download_files(copy_from_url, path, unzip=True)
